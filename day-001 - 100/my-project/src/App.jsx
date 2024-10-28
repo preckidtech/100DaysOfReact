3ffeb860be124d65f743ddd17966a66429a2d650
Oyebode Precious <preckidtech@gmail.com>
2024-10-28 16:08:15 +0100

    async function & state management

--- day-001 - 100/my-project/src/App.jsx
+++ day-001 - 100/my-project/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [advice, setAdvice] = useState("")
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <section className="bg-pink-800 p-4 m-auto mt-52 w-3/5 rounded-lg grid justify-center place-items-center">
+<h1 className="text-xl font-bold m-5 text-black text-center">ADVISE APP GENERATOR</h1>
+        <h2 className="text-white text-lg">{advice}</h2>
+        <button
+          onClick={getAdvice}
+          className="border-2 border-black rounded-lg text-white bg-blue-500 py-2 px-4"
+        >
+          Get Advise
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </section>
     </>
-  )
+  );
 }
-
-export default App

de6085dce1490a448858e36625267ffcfaaa8037
Oyebode Precious <preckidtech@gmail.com>
2024-11-04 00:03:18 +0100

    async function day 7

--- day -007 & 008/script.js
+++ day -007 & 008/script.js
@@ -50,4 +50,8 @@ booksAfterDelete;
 
 async function getTodos() {
   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+
+  return data;
 }
